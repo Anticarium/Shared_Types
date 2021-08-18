b8e976e6715accf309519c6210222fdc4d010c12
Onii-Chaan <martinnc@inbox.lv>
2021-08-18 19:08:35 +0300

    Finished for Control serializer

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+//#include ""
 
 int main(int argc, char* argv[]) {
     std::cout << "Hello, world!" << std::endl;
--- src/shared_types/serializers/ControlSerializer.hpp
+++ src/shared_types/serializers/ControlSerializer.hpp
@@ -2,5 +2,21 @@
 #include "models/Control.h"
 #include <nlohmann/json.hpp>
 
-inline void from_json(const nlohmann::json j, shared_types::Control& x) {
+inline void from_json(const nlohmann::json& j, shared_types::Control& x) {
+    nlohmann::json controlJson = j.at("Control");
+    x.setIsAuto(controlJson["auto_status"].get<bool>());
+    x.setIsHeating(controlJson["heat_status"].get<bool>());
+    x.setIsRaining(controlJson["rain_status"].get<bool>());
+    x.setLightPercentage(controlJson["light_percentage"].get<int>());
+    x.setWindPercentage(controlJson["wind_percentage"].get<int>());
+}
+
+inline void to_json(nlohmann::json& j, const shared_types::Control& x) {
+    nlohmann::json controlJson;
+    controlJson["auto_status"]      = x.isAuto();
+    controlJson["heat_status"]      = x.isHeating();
+    controlJson["rain_status"]      = x.isRaining();
+    controlJson["light_percentage"] = x.getLightPercentage();
+    controlJson["wind_percentage"]  = x.getWindPercentage();
+    j["Control"]                    = controlJson;
 }

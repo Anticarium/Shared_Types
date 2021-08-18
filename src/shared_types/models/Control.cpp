29a5918a7d683710a3cc9da9bacad2c4123e4777
Onii-Chaan <martinnc@inbox.lv>
2021-08-18 16:20:49 +0300

    Control getter and setters

---
+++ src/shared_types/models/Control.cpp
@@ -0,0 +1,45 @@
+#include "Control.h"
+
+
+namespace shared_types {
+bool Control::isRaining() const {
+    return isRaining_;
+}
+
+void Control::setIsRaining(bool newIsRaining) {
+    isRaining_ = newIsRaining;
+}
+
+bool Control::isHeating() const {
+    return isHeating_;
+}
+
+void Control::setIsHeating(bool newIsHeating) {
+    isHeating_ = newIsHeating;
+}
+
+int Control::getLightPercentage() const {
+    return lightPercentage_;
+}
+
+void Control::setLightPercentage(int newLightPercentage) {
+    lightPercentage_ = newLightPercentage;
+}
+
+int Control::getWindPercentage() const {
+    return windPercentage_;
+}
+
+void Control::setWindPercentage(int newWindPercentage) {
+    windPercentage_ = newWindPercentage;
+}
+
+bool Control::isAuto() const {
+    return isAuto_;
+}
+
+void Control::setIsAuto(bool newIsAuto) {
+    isAuto_ = newIsAuto;
+}
+
+} // namespace shared_types
--- src/shared_types/models/Control.h
+++ src/shared_types/models/Control.h
@@ -1,5 +1,24 @@
 #pragma once
 
 namespace shared_types {
-class Control {};
+class Control {
+  public:
+    bool isRaining() const;
+    void setIsRaining(bool newIsRaining);
+    bool isHeating() const;
+    void setIsHeating(bool newIsHeating);
+    int getLightPercentage() const;
+    void setLightPercentage(int newLightPercentage);
+    int getWindPercentage() const;
+    void setWindPercentage(int newWindPercentage);
+    bool isAuto() const;
+    void setIsAuto(bool newIsAuto);
+
+  private:
+    bool isRaining_      = false;
+    bool isHeating_      = false;
+    int lightPercentage_ = 0;
+    int windPercentage_  = 0;
+    bool isAuto_         = false;
+};
 } // namespace shared_types

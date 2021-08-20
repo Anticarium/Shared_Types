ec0f108ce47b006742edb870cd0366ef71ce1061
Onii-Chaan <martinnc@inbox.lv>
2021-08-20 08:56:05 +0300

    Removed quotes from CMakeLists.txt

---
+++ tests/test_serializers/ControlSerializerTest.cpp
@@ -0,0 +1,8 @@
+#include <gtest/gtest.h>
+#include <shared_types/ControlSerializer.hpp>
+
+TEST(TestControlSerializer, TestSerialization) {
+}
+
+TEST(TestControlSerializer, TestDeserialization) {
+}

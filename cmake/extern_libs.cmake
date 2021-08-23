4ea317d01da6fa6ace2edfb1c562ab36584c0f1a
Onii-Chaan <martinnc@inbox.lv>
2021-08-23 11:47:35 +0300

    Moved gtest and qt6 core includes to .cmake

--- cmake/extern_libs.cmake
+++ cmake/extern_libs.cmake
@@ -2,3 +2,22 @@ function(ADDJSON TARGET_NAME)
 	find_package(nlohmann_json CONFIG REQUIRED)
 	target_link_libraries(${TARGET_NAME} PRIVATE nlohmann_json nlohmann_json::nlohmann_json)
 endfunction()
+
+function(ADDGTEST TARGET_NAME)
+	include(FetchContent)
+	FetchContent_Declare(
+	  googletest
+	  URL https://github.com/google/googletest/archive/2f80c2ba71c0e8922a03b9b855e5b019ad1f7064.zip
+	)
+	set(gtest_force_shared_crt ON CACHE BOOL "" FORCE)
+	FetchContent_MakeAvailable(googletest)
+	enable_testing()
+	target_link_libraries(${TARGET_NAME} PRIVATE gtest_main)
+	include(GoogleTest)
+	gtest_discover_tests(${TARGET_NAME})
+endfunction()
+
+function(ADDQT6CORE TARGET_NAME)
+	find_package(Qt6 COMPONENTS Core REQUIRED)
+	target_link_libraries(${TARGET_NAME} PRIVATE Qt6::Core)
+endfunction()

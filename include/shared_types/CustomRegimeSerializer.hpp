c1482321262c73529671b29a3dfb7133f11531dd
Onii-Chaan <martinnc@inbox.lv>
2021-08-31 20:21:39 +0300

    Finished adding CustomRegime

--- include/shared_types/CustomRegimeSerializer.hpp
+++ include/shared_types/CustomRegimeSerializer.hpp
@@ -1,18 +1,22 @@
 #pragma once
-//#include <nlohmann/json.hpp>
-//#include <shared_types/CustomRegime.h>
-//#include <shared_types/RegimePropertySerializer.hpp>
+#include <nlohmann/json.hpp>
+#include <shared_types/CustomRegime.h>
+#include <shared_types/RegimePropertySerializer.hpp>
 
-// namespace nlohmann {
-// inline void from_json(const json& j, shared_types::CustomRegime& x) {
-//    x.setValue(j.at("value").get<int>());
-//    x.setIsRandom(j.at("is_random").get<bool>());
-//    x.setRange(j.at("range").get<shared_types::RegimeRange>());
-//}
+namespace nlohmann {
+inline void from_json(const json& j, shared_types::CustomRegime& x) {
+    x.setTemperature(j.at("temperature").get<float>());
+    x.setName(QString::fromStdString(j.at("name").get<std::string>()));
+    x.setRain(j.at("rain").get<shared_types::RegimeProperty>());
+    x.setWind(j.at("wind").get<shared_types::RegimeProperty>());
+    x.setLight(j.at("light").get<shared_types::RegimeProperty>());
+}
 
-// inline void to_json(json& j, const shared_types::CustomRegime& x) {
-//    j["value"]     = x.value();
-//    j["is_random"] = x.isRandom();
-//    j["range"]     = x.getRange();
-//}
-//} // namespace nlohmann
+inline void to_json(json& j, const shared_types::CustomRegime& x) {
+    j["temperature"] = x.getTemperature();
+    j["name"]        = x.getName().toStdString();
+    j["rain"]        = x.getRain();
+    j["wind"]        = x.getWind();
+    j["light"]       = x.getLight();
+}
+} // namespace nlohmann
